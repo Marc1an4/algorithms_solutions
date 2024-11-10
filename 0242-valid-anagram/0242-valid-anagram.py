894188c85fd5238dcb89a78abb9686b1ffbf1b1e
Mariana Rodríguez <juliethmariana4321@gmail.com>
2024-11-10 02:12:51 -0500

    Time: 15 ms (49.32%), Space: 16.9 MB (72.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,10 +7,4 @@ class Solution:
         for i in range(len(s)):
             s_hash[s[i]] = s_hash.get(s[i], 0) + 1
             t_hash[t[i]] = t_hash.get(t[i], 0) + 1
-        
-        for i in set(s):
-            s_curr = s_hash.get(i, 0)
-            t_curr = t_hash.get(i, 0)
-            if s_curr != t_curr:
-                return False
-        return True
+        return s_hash == t_hash
